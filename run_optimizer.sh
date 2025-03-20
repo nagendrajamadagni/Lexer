71883e31dc1fe4a940508cf95b9502c27b724887
Nagendra Kumar Jamadagni <nagendrajamadagni@gmail.com>
2025-03-19 23:55:46 -0700

    Added move commands to run optimizer script

--- run_optimizer.sh
+++ run_optimizer.sh
@@ -18,3 +18,5 @@ docker run -it --rm \
     -w /app \
     rust-valgrind-profiler valgrind --tool=callgrind --cache-sim=yes ./target/release/Lexer "a(b|c)*"
 
+mv massif.out.1 ./massif_output/massif_$(date +"%d_%m_%Y_%H_%M_%S").out
+mv callgrind.out.1 ./callgrind_output/callgrind_$(date +"%d_%m_%Y_%H_%M_%S").out
